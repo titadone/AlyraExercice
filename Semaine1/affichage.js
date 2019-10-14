f81e9415f8a65cfdaa2b2d863314828e0c9a0b82
titadone <bloum93200@gmail.com>
2019-10-14 14:56:52 +0200

    devine chiffre

--- Semaine1/affichage.js
+++ Semaine1/affichage.js
@@ -1,2 +1,4 @@
-console.log("Ceci est un text log");
-console.log(Math.random());
+console.log("Ceci est un text log")
+console.log(Math.random())
+let numeroAleatoire = Math.floor(Math.random()*10)
+console.log('le nombre aleatoire ${numeroAleatoire}')
---
+++ Semaine1/devinette.js
@@ -0,0 +1,15 @@
+const readline = require("readline")
+const rli = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+})
+console.log("Deviner le numero entre 0 et 10")
+let solution = Math.floor(Math.random()*10+1)
+rli.on("line",(userinput)=>{
+    console.log("Vous avez entré:",userinput, "essaye encore !")
+    if (userinput == solution){
+        console.log("Trouvé !")
+        rli.close()
+    }
+
+})
