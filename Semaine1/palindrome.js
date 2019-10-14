0f7fd9593b9c158f95f00f24d16661d25b004f44
titadone <bloum93200@gmail.com>
2019-10-14 17:01:09 +0200

    premiere journee

--- Semaine1/palindrome.js
+++ Semaine1/palindrome.js
@@ -1,4 +1,4 @@
-
+//Mode trivial
 function firstpalindrome(chaine){
     for (let i = 0; i < chaine.length/2; i++) {
         console.log(chaine[i])
@@ -10,6 +10,7 @@ function firstpalindrome(chaine){
     return true
 }
 
+//Mode recurssif
 function palindrome(mot){
     if( (mot.length == 0) || (mot.length == 1) ){
         return true
@@ -17,14 +18,7 @@ function palindrome(mot){
         return palindrome( mot.substring(1, (mot.length-1)) )
     }
     return false
-    
 }
 
-function estPalindrome(chaine){
-    if(palindrome(chaine)){
-        console.log(chaine,"est un palindrome")
-    }else{
-        console.log(chaine,"n'est PAS un palindrome")
-    }
-}
+//Affichage function
 console.log(palindrome("ANNA"))
