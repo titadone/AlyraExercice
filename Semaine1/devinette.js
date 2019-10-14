e92efabb0a8fff5bf4cfc6773aa84c863b884ef4
titadone <bloum93200@gmail.com>
2019-10-14 15:19:33 +0200

    ajout indice

--- Semaine1/devinette.js
+++ Semaine1/devinette.js
@@ -6,9 +6,18 @@ const rli = readline.createInterface({
 console.log("Deviner le numero entre 0 et 10")
 let solution = Math.floor(Math.random()*10+1)
 rli.on("line",(userinput)=>{
-    console.log("Vous avez entré:",userinput, "essaye encore !")
+    console.log(`Vous avez entré: ${userinput}, essaye encore (solution vaut ${solution})!`)
     if (userinput == solution){
         console.log("Trouvé !")
         rli.close()
+    }
+    if(userinput>10){
+        console.log("Attention le nombre est compris entre 0 et 10 uniquement")
+    }
+    if(userinput>solution){
+        console.log("Indice, la solution est plus petit que",userinput)
+    }
+    if(userinput<solution){
+        console.log("Indice la solution est plus grande que",userinput)
     }
 })
