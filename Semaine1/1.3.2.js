1a04bce8613561ebb6d99f43490b8769f3469a29
titadone <bloum93200@gmail.com>
2019-10-17 19:20:11 +0200

    update 1.3.2

--- Semaine1/1.3.2.js
+++ Semaine1/1.3.2.js
@@ -2,7 +2,6 @@ function occurence(chaine){
     let tab = new Map();
     for(let i = 0; i<chaine.length;i++){
         if(tab.has(chaine[i])){
-            
             tab.set(chaine[i],tab.get(chaine[i])+1)
         }else{
             tab.set(chaine[i],1)
