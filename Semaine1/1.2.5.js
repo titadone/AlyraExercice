a4fce92418e04382e415d14796ee22d21610cadd
titadone <bloum93200@gmail.com>
2019-10-18 16:49:42 +0200

    update arbre

--- Semaine1/1.2.5.js
+++ Semaine1/1.2.5.js
@@ -6,6 +6,15 @@ class Noeud {
         this.parent = undefined;
     }
     
+    ajouter(valeur) {
+        if (this.gauche == undefined)
+            this.gauche = new Noeud(valeur)
+        else if (this.droite == undefined)
+            this.droite = new Noeud(valeur)
+        else 
+            this.gauche.ajouter(valeur)
+      }
+
     // Affiche la valeur du noeud et la valeur de ses deux enfants et de son parent
     toString() {
         var out = "Noeud " + this.valeur + ":  L";
@@ -19,6 +28,15 @@ class Noeud {
         this.parent === undefined ? out += "-" : out += this.parent.valeur;
         log(out);
     }
+
+    afficherSousArbre() {
+    let sousArbre = this.valeur.toString()
+    if(this.gauche)
+      sousArbre += '('+ this.gauche.afficherSousArbre() +')'
+    if(this.droite)
+      sousArbre += '('+ this.droite.afficherSousArbre() +')'
+    return  sousArbre
+  }
 }
 class Arbre {
     constructor() {
@@ -27,31 +45,43 @@ class Arbre {
     
     //Méthode pour trouver une valeur donnée dans un arbre binaire de recherche
     trouverNoeud(valeur) {
-        while(){
-
+        let sous = this.racine
+        while(sous.gauche === undefined && sous.droite === undefined){
+            if(sous.valeur == valeur){
+                return sous
+            }if(sous.gauche != undefined){
+                sous.gauche.trouverNoeud(valeur)
+            }
         }
-        return 
         
     }
     
     //Méthode pour ajouter un noeud
     ajouterNoeud(valeur) {
         //Si arbre vide
-        if (this.racine == undefined ){
+        if (this.racine === undefined ){
             this.racine = new Noeud(valeur)
         }
         else{
-            if(this.racine.gauche == undefined){
-                this.racine.gauche = new Noeud(valeur)
-            }else if(this.racine.droit == undefined){
-                this.racine.droit = new Noeud(valeur)
-            }
-            
+            this.racine.ajouter(valeur)
         }
     }
     
     //Méthode pour supprimer un noeud
     supprimerNoeud(valeur) {
+        sousAr = this.racine
+        //feuille simple
+        if(sousAr.gauche === undefined && sousAr.droite === undefined){
+            if(sousAr.valeur == valeur){
+                sousAr.valeur = undefined
+            }
+        }
+        //un seul fils
+        if(sousAr.gauche !== undefined && sousAr.droite === undefined){
+            if(sousAr.valeur == valeur){
+                sousAr.valeur = sousAr.gauche.valeur
+            }
+        }
         if(this.racine.valeur == valeur){
             this.racine.valeur = undefined
         }else{
@@ -61,9 +91,21 @@ class Arbre {
     
     //Méthode pour afficher l’arbre selon un parcours infixe
     //Cette méthode doit retournée un tableau contenant la valeur des noeuds
+
     infixe() {
-        let tableau
-        
+        let sousArbre = this.racine
+        let arbreS = sousArbre.valeur
+        while(sousArbre != undefined){
+            if(sousArbre.gauche){
+            arbreS += this.gauche.valeur
+            sousArbre = this.gauche.afficherSousArbre()
+
+            }else{
+                arbreS += this.parent.droite.valeur
+                sousArbre = this.parent.droite.afficherSousArbre()
+            }
+        }
+        return arbreS
     }
     
     //Méthode pour afficher la valeur d'un noeud à partir de sa valeur
@@ -71,6 +113,10 @@ class Arbre {
         let noeud = this.trouverNoeud(valeur);
         if (noeud !== undefined) noeud.toString();
     }
+
+    afficherArbre(){
+        return this.racine.afficherSousArbre()
+      }
 }
 
 let a = new Arbre();
@@ -90,6 +136,6 @@ a.ajouterNoeud(31);
 a.ajouterNoeud(35);
 a.ajouterNoeud(32);
 
-console.log(a.racine.toString);
+console.log(a.afficherArbre());
 
-//console.log(a.infixe());
+console.log(a.infixe());
