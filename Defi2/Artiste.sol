1199b88a07bb203d604b951be4de015909f3c2ee
titadone <bloum93200@gmail.com>
2019-11-08 10:46:54 +0100

    update

--- Defi2/Artiste.sol
+++ Defi2/Artiste.sol
@@ -1,4 +1,4 @@
-pragma solidity ^0.5.7;
+pragma solidity ^0.5.11;
 //pragma experimental ABIEncoderV2;
 
 contract ArtisteContract{
@@ -10,15 +10,29 @@ contract ArtisteContract{
 
     mapping(address => Artiste) list_artiste;
     address[] list_ban;
+    address owner;
+
+    constructor(){
+        owner = msg.sender;
+    }
+
+    modifier admin(){
+        require(msg.sender == owner);
+    }
 
     function inscription(string memory _nom) public{
+        require(contains(msg.sender) == true , "Vous êtes déjà banni!");
+
         list_artiste[msg.sender].reputation = 1;
         list_artiste[msg.sender].nom = _nom;
     }
 
-    function ban(address ban_user) public{
+    function ban(address ban_user) public admin{
         list_artiste[ban_user].reputation = 0;
         list_ban.push(ban_user);
     }
 
+    function contains(address is_ban) public (bool){
+        return list_ban[is_ban];
+    }
 }
